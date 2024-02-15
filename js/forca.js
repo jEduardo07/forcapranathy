6d3c1bb4d3b8806102c0fb2e54831e1a50a3308f
Jeruza2021 <jeje.maria37@gmail.com>
2024-02-15 18:10:33 -0300

    jogo da forca

---
+++ js/forca.js
@@ -0,0 +1,290 @@
+let jogarNovamente = true;
+let tentativas = 6;
+let listaDinamica = [];
+let palavraSecretaCategoria;
+let palavraSecretaSorteada;
+let palavras = [];
+let jogoAutomatico = true;
+
+carregaListaAutomatica();
+
+criarPalavraSecreta();
+function criarPalavraSecreta(){
+    const indexPalavra = parseInt(Math.random() * palavras.length)
+    
+    palavraSecretaSorteada = palavras[indexPalavra].nome;
+    palavraSecretaCategoria = palavras[indexPalavra].categoria;
+
+    // console.log(palavraSecretaSorteada);
+}
+
+montarPalavraNaTela();
+function montarPalavraNaTela(){
+    const categoria = document.getElementById("categoria");
+    categoria.innerHTML = palavraSecretaCategoria;
+
+    const palavraTela = document.getElementById("palavra-secreta");
+    palavraTela.innerHTML = "";
+    
+    for(i = 0; i < palavraSecretaSorteada.length; i++){  
+        if(listaDinamica[i] == undefined){
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
+            }
+            else{
+                listaDinamica[i] = "&nbsp;"
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }     
+        }
+        else{
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
+            }
+            else{
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }    
+        }
+    }   
+}
+
+function verificaLetraEscolhida(letra){
+    document.getElementById("tecla-" + letra).disabled = true;
+    if(tentativas > 0)
+    {
+        mudarStyleLetra("tecla-" + letra, false);
+        comparalistas(letra);
+        montarPalavraNaTela();
+    }    
+}
+
+function mudarStyleLetra(tecla, condicao){
+    if(condicao == false)
+    {
+        document.getElementById(tecla).style.background = "#C71585";
+        document.getElementById(tecla).style.color = "#ffffff";
+    }
+    else{
+        document.getElementById(tecla).style.background = "#008000";
+        document.getElementById(tecla).style.color = "#ffffff";
+    }
+
+    
+}
+
+function comparalistas(letra){
+    const pos = palavraSecretaSorteada.indexOf(letra)
+    if(pos < 0){
+        tentativas--
+        carregaImagemForca();
+
+        if(tentativas == 0){
+            abreModal("OPS!", "A resposta era obvia ... Obviamente a pessoa é a <br>" + palavraSecretaSorteada);
+            piscarBotaoJogarNovamente(true);
+        }
+    }
+    else{
+        mudarStyleLetra("tecla-" + letra, true);
+        for(i = 0; i < palavraSecretaSorteada.length; i++){
+            if(palavraSecretaSorteada[i] == letra){
+                listaDinamica[i] = letra;
+            }
+        }
+    }
+    
+    let vitoria = true;
+    for(i = 0; i < palavraSecretaSorteada.length; i++){
+        if(palavraSecretaSorteada[i] != listaDinamica[i]){
+            vitoria = false;
+        }
+    }
+
+    if(vitoria == true)
+    {
+        abreModal("PARABÉNS!", "Você é incrivel, vida");
+        tentativas = 0;
+        piscarBotaoJogarNovamente(true);
+    }
+}
+
+// async function piscarBotaoJogarNovamente(){
+//     while (jogarNovamente == true) {
+//         document.getElementById("btnReiniciar").style.backgroundColor = 'red';
+//         document.getElementById("btnReiniciar").style.scale = 1.3;
+//         await atraso(500)
+//         document.getElementById("btnReiniciar").style.backgroundColor = 'yellow';
+//         document.getElementById("btnReiniciar").style.scale = 1;
+//         await atraso(500)
+//     }
+// }
+
+async function atraso(tempo){
+    return new Promise(x => setTimeout(x, tempo))     
+}
+
+function carregaImagemForca(){
+    switch(tentativas){
+        case 5:
+            document.getElementById("imagem").style.background  = "url('./img/forca01.png')";
+            break;
+        case 4:
+            document.getElementById("imagem").style.background  = "url('./img/forca02.png')";
+            break;
+        case 3:
+            document.getElementById("imagem").style.background  = "url('./img/forca03.png')";
+            break;
+        case 2:
+            document.getElementById("imagem").style.background  = "url('./img/forca04.png')";
+            break;
+        case 1:
+            document.getElementById("imagem").style.background  = "url('./img/forca05.png')";
+            break;
+        case 0:
+            document.getElementById("imagem").style.background  = "url('./img/forca06.png')";
+            break;
+        default:
+            document.getElementById("imagem").style.background  = "url('./img/forca.png')";
+            break;
+    }
+}
+
+function abreModal(titulo, mensagem){
+    let modalTitulo = document.getElementById("exampleModalLabel");
+    modalTitulo.innerText = titulo;
+
+    let modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = mensagem;
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+let bntReiniciar = document.querySelector("#btnReiniciar")
+bntReiniciar.addEventListener("click", function(){
+    jogarNovamente = false;
+    location.reload();
+});
+
+function listaAutomatica(){ // ativa o modo manual
+    if (jogoAutomatico == true) {
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-play-circle'></i>"
+        palavras = [];
+        jogoAutomatico = false;
+
+        document.getElementById("abreModalAddPalavra").style.display = "block";
+        document.getElementById("status").innerHTML = "Modo Manual";
+    }
+    else if(jogoAutomatico == false){ // ativa o modo automático
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-pause-circle'></i>"
+        jogoAutomatico = true;
+
+        document.getElementById("abreModalAddPalavra").style.display = "none";
+        document.getElementById("status").innerHTML = "Modo Automático";
+        
+    }
+}
+
+const modal = document.getElementById("modal-alerta");
+
+const btnAbreModal = document.getElementById("abreModalAddPalavra");
+btnAbreModal.onclick = function(){
+    modal.style.display = "block";
+}
+
+const btnFechaModal = document.getElementById("fechaModal");
+btnFechaModal.onclick = function(){ 
+    modal.style.display = "none";
+    document.getElementById("addPalavra").value = "";
+    document.getElementById("addCategoria").value = ""; 
+}
+
+window.onclick = function(){ 
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("addPalavra").value = "";
+        document.getElementById("addCategoria").value = ""; 
+    }  
+}
+
+function carregaListaAutomatica(){
+    palavras = [
+        palavra001 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA ESPECIAL"
+        },
+        palavra002 = {
+            nome: "NATHIELLY",
+            categoria:"A MULHER MAIS LINDA DESSE PLANETA"
+        },
+        palavra003 = {
+            nome: "NATHIELLY",
+            categoria:"PESSOA COM PERSONALIDADE UNICA"
+        },
+        palavra004 = {
+            nome: "NATHIELLY",
+            categoria:"MELHOR COMPANHIA"
+        },
+        palavra005 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA TALENTOSA"
+        },
+        palavra006 = {
+            nome: "NATHIELLY",
+            categoria:"TEM UMA PRESENÇA INCRIVEL E INSPERADORA"
+        },
+        palavra007 = {
+            nome: "NATHIELLY",
+            categoria:"A MAIS MAIS"
+        },
+        palavra008 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA QUE TRAZ PAZ"
+        },
+        palavra009 = {
+            nome: "NATHIELLY",
+            categoria:"PESSOA COM A VOZ MAIS LINDA DESSE PLANETA"
+        },
+        palavra010 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA ESTILOSA"
+        },
+        palavra009 = {
+            nome: "NATHIELLY",
+            categoria:"O AMOR DA MINHA VIDA"
+        },
+        palavra010 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA INTELIGENTE"
+        },
+        palavra011 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA QUE SEMPRE ALEGRA O AMBIENTE"
+        },
+        palavra012 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA GENTIL E SIMPATICA"
+        },
+        palavra013 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA QUE É BOA EM TUDO QUE FAZ"
+        },
+        palavra013 = {
+            nome: "NATHIELLY",
+            categoria:"UMA OTIMA CONSELHEIRA"
+        },
+        palavra014 = {
+            nome: "NATHIELLY",
+            categoria:"A PESSOA MAIS FORTE DESSE MUNDO"
+        },
+        palavra015 = {
+            nome: "NATHIELLY",
+            categoria:"UMA PESSOA QUE É ESFORÇADA"
+        },
+      
+    ];
+}
+
+
+
